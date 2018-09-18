40f988361734b67c8b7507fe8b8f2eee8e1cc9cf
Manish Kumar Patwari <manish.patwari@ETV-C02VW1YNG8WN.local>
2018-09-18 13:07:30 +0530

    added isHardwareSupported flag

--- library/core/src/main/java/com/google/android/exoplayer2/mediacodec/MediaCodecInfo.java
+++ library/core/src/main/java/com/google/android/exoplayer2/mediacodec/MediaCodecInfo.java
@@ -85,6 +85,12 @@ public final class MediaCodecInfo {
    */
   public final boolean secure;
 
+  /**
+   * True : Codec is Hardware accelerated.
+   * False : Codec is not Hardware accelerated.
+   */
+  public boolean isHardwareSupported;
+
   /** Whether this instance describes a passthrough codec. */
   public final boolean passthrough;
 
@@ -157,6 +163,11 @@ public final class MediaCodecInfo {
     adaptive = !forceDisableAdaptive && capabilities != null && isAdaptive(capabilities);
     tunneling = capabilities != null && isTunneling(capabilities);
     secure = forceSecure || (capabilities != null && isSecure(capabilities));
+    if (!name.toLowerCase().startsWith("omx.google.")) {
+      isHardwareSupported = true;
+    } else {
+      isHardwareSupported = false;
+    }
   }
 
   /**
